b2cad079749f6ab8d80bf2b2ac9b4a5a4941e9f8
Robin H. Johnson <robbat2@gentoo.org>
2015-01-20 12:02:51 -0800

    Add initial Debian support.
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- manifests/params.pp
+++ manifests/params.pp
@@ -41,12 +41,55 @@ class rt::params {
                 'priorityasstring'      => 'perl-RT-Extension-PriorityAsString',
             }
         }
+        "Debian":  {
+            $rt_version     = '4'
+            $rt_name        = "request-tracker${rt_version}"
+            $rt_prefix      = "rt${rt_version}"
+            $rt_dir         = "/etc/request-tracker${rt_version}"
+            $rt_mailgate    = "${rt_prefix}-clients"
+            $rt_grp         = "www"
+            $rt_db_mysql    = "${rt_prefix}-db-mysql"
+            $rt_db_oracle   = "${rt_prefix}-db-oracle"
+            $rt_db_postgres = "${rt_prefix}-db-postgres"
+            $rt_db_sqlite   = "${rt_prefix}-db-sqlite"
+            $rt_tool_cnf    = "/root/.rtrc"
+            $manage_repo    = true
+            #
+            $extprefix      = "${rt_prefix}-extension"
+            $ext_packages   = {
+                #'activityreports'          => 'perl-RT-Extension-ActivityReports',
+                #'assets'                   => 'perl-RT-Extension-Assets',
+                'assettracker'             => "${extprefix}-assettracker",
+                #'commandbymail'            => 'perl-RT-Extension-CommandByMail',
+                #'cookieauth'               => 'perl-RT-Authen-CookieAuth',
+                'customfieldonupdate'      => "${extprefix}-customfieldsonupdate",
+                #'externalauth'             => 'perl-RT-Authen-ExternalAuth',
+                'jsgantt'                  => "${extprefix}-jsgannt",
+                #'ldapimport'               => 'perl-RT-Extension-LDAPImport',
+                #'mergeusers'               => 'perl-RT-Extension-MergeUsers',
+                #'mobileui'                 => 'perl-RT-Extension-MobileUI',
+                #'nagios'                   => 'perl-RT-Extension-Nagios',
+                #'notifyowners'             => 'perl-RT-Extension-NotifyOwners',
+                #'priorityasstring'         => 'perl-RT-Extension-PriorityAsString',
+                #'quickupdate'              => 'perl-RT-Extension-QuickUpdate',
+                #'repliestoresolved'        => 'perl-RT-Extension-RepliesToResolved',
+                #'sla'                      => 'perl-RT-Extension-SLA',
+                # Custom:
+                'authenexternalauth'       => "${extprefix}-authenexternalauth",
+                'calendar'                 => "${extprefix}-calendar",
+                'spawnlinkedticketinqueue' => "${extprefix}-spawnlinkedticketinqueue",
+                #'xx'                       => "${extprefix}-xx",
+
+            }
+        }
+        
         default: { fail("Unsupported platform: ${::osfamily}") }
     }
     #
     if ($manage_repo) {
         case $::osfamily {
             "Suse":     { class { "rt::repo::zypprepo": } }
+            "Debian":   { } # Upstream supported
             default:    { fail("Unsupported osfamily: ${::osfamily} for module ${module_name}") }
         }
     }
