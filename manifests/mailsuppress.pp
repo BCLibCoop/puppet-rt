f32c3348ef161a61c34aca7d82a30f07e17cd92b
Daniel Mezentsev <dan@soleks.com>
2015-01-20 10:24:53 -0800

    Adds suppress all emails functionlity

---
+++ manifests/mailsuppress.pp
@@ -0,0 +1,22 @@
+#
+class rt::mailsuppress (
+    $ensure = 'present'
+     ) {
+    include rt::params
+    #
+   file { '/etc/request-tracker3.8/RT_SiteConfig.d/60-mail-suppress':
+    ensure  => 'file',
+    source  => 'puppet:///modules/rt/etc/request-tracker3.8/RT_SiteConfig.d/60-mail-suppress',
+    mode    => '0600',
+    owner   => 'root',
+    group   => 'root',
+    require => File['/etc/request-tracker3.8/RT_SiteConfig.d/'],
+  }
+   file { '/usr/share/request-tracker3.8/lib/RT/Action/SendEmail.pm':
+    ensure  => 'file',
+    source  => 'puppet:///modules/rt/usr/share/request-tracker3.8/lib/RT/Action/SendEmail.pm',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+  }
+}
