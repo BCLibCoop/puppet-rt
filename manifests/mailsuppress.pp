4374ea8fcf50451181e4821dad73a8945ed08ddb
Robin H. Johnson <robbat2@gentoo.org>
2015-01-20 12:07:00 -0800

    Remove dep for now.
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- manifests/mailsuppress.pp
+++ manifests/mailsuppress.pp
@@ -4,12 +4,12 @@ class rt::mailsuppress (
      ) {
     #
    file { '/etc/request-tracker3.8/RT_SiteConfig.d/60-mail-suppress':
-    ensure  => 'file',
+    ensure  => 'present',
     source  => 'puppet:///modules/rt/etc/request-tracker3.8/RT_SiteConfig.d/60-mail-suppress',
     mode    => '0600',
     owner   => 'root',
     group   => 'root',
-    require => File['/etc/request-tracker3.8/RT_SiteConfig.d/'],
+	#require => File['/etc/request-tracker3.8/RT_SiteConfig.d/'],
   }
    file { '/usr/share/request-tracker3.8/lib/RT/Action/SendEmail.pm':
     ensure  => 'file',
