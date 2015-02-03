d27e3c3b7611fdd4587794f8407cf4d4c5c1af90
Daniel Mezentsev <dan@soleks.com>
2015-02-03 13:25:36 -0800

    removed DefaultQueue order

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -56,12 +56,5 @@ class rt::plugins (
     #notify => Exec['update-rt-siteconfig'],
   }
 
-  concat::fragment { 'DefaultQueue':
-    target => $config_file,
-    content => "Set($DefaultQueue, 'Support');\n",
-    order   => '06',
-    #notify => Exec['update-rt-siteconfig'],
-  }
-
 }
 # vim: set ft=puppet si sts=2 et tw=80 sw=2:
